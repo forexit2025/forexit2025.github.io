02720356a7a0eafc2ac7d3f269a834dee69a0210
Amy <z5481175@ad.unsw.edu.au>
2025-06-02 21:13:14 +1000

    fixed button animation

--- src/components/YearNavigator.jsx
+++ src/components/YearNavigator.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function YearNavigator({ year, setYear }) {
   const years = () => {
-    const array = ["2025", "2026", "2027", "2028"];
+    const array = ["2025", "2026", "2027"];
     // const array = [];
     // const currYear = new Date().getFullYear();
     // for (let i = 2025; i <= currYear; i++) {
@@ -19,13 +19,15 @@ function YearNavigator({ year, setYear }) {
     <div className="flex flex-row justify-center gap-3">
       {years &&
         years().map((yr, i) => (
-          <button
-            key={i}
-            onClick={() => setYear(yr)}
-            className={`text-white ${yr === year ? "bg-[linear-gradient(#54C8D5,#399CE2)]" : "bg-[#AEAEAE]"}`}
-          >
-            {yr}
-          </button>
+          <div className="transform transition-transform duration-300 hover:scale-105">
+            <button
+              key={i}
+              onClick={() => setYear(yr)}
+              className={`text-white ${yr === year ? "bg-[linear-gradient(#54C8D5,#399CE2)]" : "bg-[#AEAEAE]"}`}
+              >
+              {yr}
+            </button>
+          </div>
         ))}
     </div>
   );
